8ea0789d3710c3b9390c9eb93238cd4b66819d05
Meghraj Modi <meghrajmodi64@gmail.com>
2023-06-23 15:26:27 +0530

    first Commint

--- src/App.js
+++ src/App.js
@@ -1,20 +1,73 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
+// Define the Resume component
+const Resume = () => {
+  // Data for the resume
+  const personalInfo = {
+    name: 'Meghraj Modi',
+    title: 'Full Stack Developer',
+    email: 'meghrajmodi.com',
+    phone: '9610404478',
+    address: '123 Main St, City, State,Bengluru'
+  };
+
+  const experience = [
+    {
+      title: 'Software Engineer',
+      company: 'ABC Company',
+      duration: '2018 - Present',
+      description: 'Implemented new features and maintained existing applications.'
+    },
+    {
+      title: 'Front-end Developer',
+      company: 'XYZ Agency',
+      duration: '2016 - 2018',
+      description: 'Developed user-friendly interfaces and optimized website performance.'
+    }
+  ];
+
+  const education = [
+    {
+      degree: 'Bachelor of Computer Application',
+      school: 'Bhupal Nobels University Udaipur (Raj.)',
+      duration: '2019- 2022'
+    }
+  ];
+
+  // Render the resume
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a>
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>{personalInfo.name}</h1>
+      <h2>{personalInfo.title}</h2>
+
+      <ul>
+        <li>Email: {personalInfo.email}</li>
+        <li>Phone: {personalInfo.phone}</li>
+        <li>Address: {personalInfo.address}</li>
+      </ul>
+
+      <h3>Experience</h3>
+      <ul>
+        {experience.map((item, index) => (
+          <li key={index}>
+            <h4>{item.title}</h4>
+            <p>{item.company} | {item.duration}</p>
+            <p>{item.description}</p>
+          </li>
+        ))}
+      </ul>
+
+      <h3>Education</h3>
+      <ul>
+        {education.map((item, index) => (
+          <li key={index}>
+            <h4>{item.degree}</h4>
+            <p>{item.school} | {item.duration}</p>
+          </li>
+        ))}
+      </ul>
     </div>
   );
-}
+};
 
-export default App;
+export default Resume;
